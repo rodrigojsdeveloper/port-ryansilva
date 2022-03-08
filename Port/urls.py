f052d12bec38ef25bc5514e48c22a81652f298e2
Ryan Silva <ryanprogrammerpy@gmail.com>
2022-03-08 10:07:12 -0300

    deploy

--- Port/urls.py
+++ Port/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('', include('cont.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
