95cf77b4b433f46ada13290a94a16b890384bfb0
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-07-24 00:26:09 -0300

    V2 initialized

--- cont/urls.py
+++ cont/urls.py
@@ -6,8 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('port/projects', views.ProjectView.as_view(), name='projects'),
-    path('port/about-me', views.about, name='about'),
-    path('port/hardskills', views.hardskills, name='hardskills')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cont/views.py
+++ cont/views.py
@@ -6,17 +6,3 @@ from .models import Projects
 
 def index(request):
     return render(request, 'index.html')
-
-
-class ProjectView(ListView):
-    model = Projects
-    template_name = 'index_projects.html'
-    paginate_by = 3
-    context_object_name = 'projects'
-
-def about(request):
-    return render(request, 'index_about.html')
-
-
-def hardskills(request):
-    return render(request, 'index_hardskills.html')
