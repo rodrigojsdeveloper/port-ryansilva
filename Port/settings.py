67c0ec30c5e6bf0b3fbb4a01e71fd55a57101eff
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-07-30 17:30:06 -0300

    upgrade style, debug and url

--- Port/settings.py
+++ Port/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wx-0^(45ke4%zk$&x^2y+i%&46_gt*6lre4v)y$m9g^o--9-24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['portryan-env-1.eba-r3mjces3.us-east-1.elasticbeanstalk.com']
 
@@ -139,8 +139,3 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-# CSRF_COOKIE_HTTPONLY = True
-
-# SECURE_HSTS_PRELOAD = True
-# SECURE_SSL_REDIRECT = True
-	
