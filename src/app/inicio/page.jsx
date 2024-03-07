e2231b6fc145e6d17391b48e5212d92ba3986ac2
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2024-03-06 23:38:44 -0300

    Feat: @component/Button

--- src/app/inicio/page.jsx
+++ src/app/inicio/page.jsx
@@ -1,14 +1,17 @@
 'use client'
 
 import Button from "@/components/Button";
+import BookIcon from "@/components/svgs/BookIcon";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Pagina home</h1>
-      <Button onClick={() => {}}>
-        Proximo
-      </Button>
+      <Button
+        label="Sobre mim"
+        icon={<BookIcon/>}
+        onClick={() => {}}
+      />
     </main>
   );
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,20 +1,24 @@
-import React from "react"
+import React from "react";
 
-const Button = ({
-  children,
-  onClick = () => {},
-  disabled = false,
-}) => {
-  return <>
-		<button 
-			className="border border-black mx-1 my-3 py-1 px-5 rounded-md font-normal"
-			onClick={() => onClick()}
-			disabled={disabled}
-		> 
-			<span>
-				{children}
-			</span>
-		</button>
-	</>
-}
-export default Button
+const Button = ({ label, icon, onClick = () => { }, className  = "", disabled = false }) => {
+	return (
+		<>
+			<button
+				className={`border border-black mx-1 my-3 py-2 px-5 rounded-md font-normal ${className}`}
+				onClick={() => onClick()}
+				disabled={disabled}
+			>
+				<span className="flex gap-2 items-center justify-center">
+					{icon && icon}
+					<div className="group relative overflow-hidden">
+						<span class="invisible">{label}</span>
+						<span class="absolute left-0 top-0 cursor-pointer text-sm text-foreground-muted transition-transform duration-500 ease-in-out hover:duration-300 group-hover:-translate-y-full sm:text-base">{label}</span>
+						<span class="absolute left-0 top-0 translate-y-full text-sm text-foreground-muted transition-transform duration-500 ease-in-out hover:duration-300 group-hover:translate-y-0 sm:text-base">{label}</span>
+					</div>
+				</span>
+			</button >
+		</>
+	);
+};
+
+export default Button;
