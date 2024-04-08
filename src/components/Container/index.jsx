457031aac871a4ebf8577fbbda0414b4a4cafc15
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2024-04-08 02:45:53 -0300

    Refactor: Ajustando container + shape

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -5,12 +5,13 @@ import { useInView } from 'react-intersection-observer';
 import Image from 'next/image';
 import Button from '../Button';
 import BranchIcon from '../Svgs/BranchIcon';
+import Shape1Icon from '../Svgs/Shape1Icon';
 
 
 const Container = () => {
 
   const { ref, inView } = useInView({
-    triggerOnce: true, // A animação ocorrerá apenas uma vez
+    triggerOnce: true, // A animação ocorrerá apenas uma vez // testar false e tirar fowards do css
     threshold: 0.4, // Trigger quando 50% do item estiver visível
   });
 
@@ -50,6 +51,9 @@ const Container = () => {
             <h1 className="text-3xl md:text-5xl font-medium text-white-primary">Soluções funcionais e produtivas</h1>
           </div>
         </div>
+        <div className='relative top-[10em] left-32'>
+          <Shape1Icon/>
+        </div>
       </div>
       <div ref={ref} className={`opacity-0 ${inView ? `animate-slideInRight` : ''} transform`}>
         <img className='relative bottom-[70px] rounded-xl border border-gray-700' src='/ide.png' alt='image'/>
@@ -64,10 +68,18 @@ const Container = () => {
             <div className='w-[35em] mb-2'>
               <p className='text-2xl font-medium text-gray-primary'><span className='text-white-primary'>GitHub Copilot</span> empowers developers to complete tasks 55% faster with contextualized AI coding assistance across workflows.</p>
             </div>
-            <Button label='Explore GitHub Copilot '/>
+            <Button label='Explore GitHub Copilot'/>
           </div>
-          <div className='relative left-[-99px]'>
+          <div className='flex items-center relative left-[-99px]'>
             <BranchIcon/>
+            <div>
+              <span className='text-xs font-semibold text-green-primary border border-green-primary rounded-full py-[1px] px-2'>Did you know?</span>
+              <div className='my-5 w-[35em]'>
+                <h1 className="text-3xl md:text-6xl font-medium text-green-primary">22% increase</h1>
+                <h1 className="text-xl md:text-2xl font-medium text-white-primary">in developer productivity after three years with GitHub</h1>
+              </div>
+              <Button label='Read the report '/>
+            </div>
           </div>
         </div>
       </div>
