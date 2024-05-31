1f11f53078b4a19859fb09579e920dfe647525d3
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2024-05-31 00:20:51 -0300

    Style: Nova ide /sobre mim

--- src/components/ImageContainer/index.jsx
+++ src/components/ImageContainer/index.jsx
@@ -35,7 +35,7 @@ const ImageContainer = ({ src }) => {
   return (
     <div ref={ref} className={`opacity-0 ${animationClass} transform mx-4`}>
       <img className='z-10 relative bottom-[70px] rounded-xl border border-gray-700' src={src} alt='image' />
-      <SubCard title={'Meu serviços'} className={'absolute top-[27em] left-[44em] w-[33em] z-20'}>
+      <SubCard title={'Meu serviços'} className={'absolute top-[30em] left-[44em] w-[33em] z-20'}>
         <div className="flex items-start gap-3 text-blue-primary border-b border-gray-600 py-4">
           <CheckIcon width={'45'} height={'20'}/>
           <div>
