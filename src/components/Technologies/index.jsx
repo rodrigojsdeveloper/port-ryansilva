4930caeff6339e4340781c6cc58ae1990cc1ce96
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2024-06-02 18:12:46 -0300

    Refactor: V1 skils

--- src/components/Technologies/index.jsx
+++ src/components/Technologies/index.jsx
@@ -13,37 +13,35 @@ import SubCard from '../SubCard';
 const Technologies = () => {
   return (
     <div className='max-sm:hidden'>
-      <SubCard title={'Meu serviços'} className={'absolute top-[10em] left-[40em] z-20'}>
-        <div className='flex items-center justify-center gap-10 p-5'>
-          <div className='flex flex-col items-center justify-center gap-2'>
-            <ItemTech animationSide='left'>
-              <SalesforceIcon />
-            </ItemTech>
-            <ItemTech animationSide='left'>
-              <AzureIcon />
-            </ItemTech>
-          </div>
-          <div className='flex flex-col justify-center'>
-            <ItemTech animationSide='top'>
-              <PhpIcon />
-            </ItemTech>
-            <ItemTech animationSide='left'>
-              <PythonIcon />
-            </ItemTech>
-            <ItemTech animationSide='bottom'>
-              <ReactIcon />
-            </ItemTech>
-          </div>
-          <div className='flex flex-col items-center justify-center gap-2'>
-            <ItemTech animationSide='right'>
-              <DockerIcon />
-            </ItemTech>
-            <ItemTech animationSide='right'>
-              <TypeScriptIcon />
-            </ItemTech>
-          </div>
+      <div className='flex items-center justify-center gap-10 p-5'>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <ItemTech animationSide='left'>
+            <SalesforceIcon />
+          </ItemTech>
+          <ItemTech animationSide='left'>
+            <AzureIcon />
+          </ItemTech>
         </div>
-      </SubCard>
+        <div className='flex flex-col justify-center'>
+          <ItemTech animationSide='top'>
+            <PhpIcon />
+          </ItemTech>
+          <ItemTech animationSide='left'>
+            <PythonIcon />
+          </ItemTech>
+          <ItemTech animationSide='bottom'>
+            <ReactIcon />
+          </ItemTech>
+        </div>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <ItemTech animationSide='right'>
+            <DockerIcon />
+          </ItemTech>
+          <ItemTech animationSide='right'>
+            <TypeScriptIcon />
+          </ItemTech>
+        </div>
+      </div>
     </div>
   );
 };
